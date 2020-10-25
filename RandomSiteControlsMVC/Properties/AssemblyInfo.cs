2dfa043730b6586ed9e08bc6469b6a380a6592f4
Steve Scott <steve@sitefinitysteve.com>
2020-10-25 19:46:15 -0400

    Bump version

--- RandomSiteControlsMVC/Properties/AssemblyInfo.cs
+++ RandomSiteControlsMVC/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("13.1.7400.22")]
-[assembly: AssemblyFileVersion("13.1.7400.22")]
+[assembly: AssemblyVersion("13.1.7400.23")]
+[assembly: AssemblyFileVersion("13.1.7400.23")]
 
 //INSTALLER
 [assembly: PreApplicationStartMethod(typeof(Installer), "PreApplicationStart")]
